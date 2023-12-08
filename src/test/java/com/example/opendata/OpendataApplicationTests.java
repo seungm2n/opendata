b5eb93fa11c888161d257b1ef73075b8ae551fb0
seungm2n2 <seungm2n2@gmail.com>
2023-12-08 21:50:03 +0900

    Initial commit

---
+++ src/test/java/com/example/opendata/OpendataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.opendata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpendataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
