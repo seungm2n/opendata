9b9b2fb64e6e4d1067a2be351973059dc87f053c
seungm2n2 <seungm2n2@gmail.com>
2023-12-09 22:01:28 +0900

    .sh 작성

--- src/main/java/com/example/controller/index.java
+++ src/main/java/com/example/controller/index.java
@@ -2,12 +2,13 @@ package com.example.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class index {
 
     @GetMapping("/")
     public String index() {
-        return "index 호출 됨";
+        return "index 호출 됨.";
     }
 }
