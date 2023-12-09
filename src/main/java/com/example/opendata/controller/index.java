f672e872fdc82a95964a84b5ebcf8302a51653d0
seungm2n2 <seungm2n2@gmail.com>
2023-12-09 22:03:53 +0900

    경로 수정

--- src/main/java/com/example/opendata/controller/index.java
+++ src/main/java/com/example/opendata/controller/index.java
@@ -1,4 +1,4 @@
-package com.example.controller;
+package com.example.opendata.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
